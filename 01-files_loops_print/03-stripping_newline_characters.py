26b18dc768eee42660466c77a54fc653041ffc9e
Kevin Kirsche <kevin.kirsche@verizon.com>
2017-03-13 10:42:28 -0400

    Add powerpoint

--- 01-files_loops_print/03-stripping_newline_characters.py
+++ 01-files_loops_print/03-stripping_newline_characters.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
     with open("fixtures/ip_addresses.txt", "r") as file_handler:
         for line in file_handler:
-            no_newline = line.strip("\n")
+            no_newline = line.strip()
             print(no_newline)
--- 01-files_loops_print/05-putting_it_all_together.py
+++ 01-files_loops_print/05-putting_it_all_together.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+prefix_list_tmpl = "set policy-options prefix-list 64510-customers {ip}/32"
 
 if __name__ == "__main__":
-    prefix_list_tmpl = "set policy-options prefix-list 64510-customers {!s}/32"
     with open("fixtures/ip_addresses.txt", "r") as file_handler:
         for line in file_handler:
-            ip_address = line.strip("\n")
-            full_string = prefix_list_tmpl.format(ip_address)
+            ip_address = line.strip()
+            full_string = prefix_list_tmpl.format(ip=ip_address)
             print(full_string)
